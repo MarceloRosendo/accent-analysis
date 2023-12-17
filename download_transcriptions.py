0cf98d3af931049974d303b3d1d7cf466188235b
MarceloRosendo <marcelomax159@gmail.com>
2023-12-17 19:49:38 -0300

    testing with new class

--- download_transcriptions.py
+++ download_transcriptions.py
@@ -6,6 +6,7 @@ def download_transcriptions(path, video_id_list):
         try:
             response = pd.DataFrame(YouTubeTranscriptApi.get_transcript(id, languages=['pt']))
             response.to_json(f'data/{path}/{index}.json')
+            print('transcripting', index)
         except:
             continue
 
@@ -14,15 +15,22 @@ def get_list_ids_from_file(src):
         lines = f.readlines()
         return list(map(lambda line: line.replace('\n', ''),lines))
 
-accent_list_1 = get_list_ids_from_file('subject_accent_1_ids')
-accent_list_2 = get_list_ids_from_file('subject_accent_1_2_ids')
-accent_list_3 = get_list_ids_from_file('subject_accent_2_ids')
-accent_list_4 = get_list_ids_from_file('subject_accent_2_2_ids')
+#baiano
+#accent_list_1 = get_list_ids_from_file('subject_accent_1_ids')
+#accent_list_2 = get_list_ids_from_file('subject_accent_1_2_ids')
 
-fluminense = accent_list_1 + accent_list_2
-baiano = accent_list_3 + accent_list_4
-#mineiro = ['Z8wnqhpO3Hs']
+#fluminense
+#accent_list_3 = get_list_ids_from_file('subject_accent_2_ids')
+#accent_list_4 = get_list_ids_from_file('subject_accent_2_2_ids')
 
-download_transcriptions('fluminense', fluminense)
-download_transcriptions('baiano', baiano)
-#download_transcriptions('mineiro', mineiro)
+#mineiro
+accent_list_5 = get_list_ids_from_file('subject_accent_3_ids')
+accent_list_6 = get_list_ids_from_file('subject_accent_3_2_ids')
+
+#fluminense = accent_list_1 + accent_list_2
+#baiano = accent_list_3 + accent_list_4
+mineiro = accent_list_5 + accent_list_6
+
+#download_transcriptions('fluminense', fluminense)
+#download_transcriptions('baiano', baiano)
+download_transcriptions('mineiro', mineiro)
